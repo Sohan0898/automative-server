b0204dbc646d3f0976d22035d8328880a218f5e9
sohan0898 <shuhanur.rahman08@gmail.com>
2023-10-18 21:42:27 +0600

    create post method to  add product insert in  database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 
 const app = express();
@@ -11,8 +12,8 @@ app.use(cors());
 app.use(express.json());
   
 
- const { MongoClient, ServerApiVersion } = require('mongodb');
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8p2aqm7.mongodb.net/?retryWrites=true&w=majority`;
+
  
  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
  const client = new MongoClient(uri, {
@@ -26,6 +27,17 @@ app.use(express.json());
  async function run() {
    try {
      // Connect the client to the server	(optional starting in v4.7)
+    const productCollection = client.db('ProductsDB').collection('products');
+
+    app.post('/products', async (req, res)=>{
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result);
+    });
+
+
+
      await client.connect();
      // Send a ping to confirm a successful connection
      await client.db("admin").command({ ping: 1 });
